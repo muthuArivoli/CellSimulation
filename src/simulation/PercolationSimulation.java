aa04777fdd072824fca780e19b3d471f7e3850e2
Muthu Arivoli <ma381@duke.edu>
2020-02-02 01:36:45 -0500

    add Percolation Simulation

---
+++ src/simulation/PercolationSimulation.java
@@ -0,0 +1,25 @@
+package simulation;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class PercolationSimulation extends Simulation{
+    protected void getNextState(Cell cell, List<Cell> neighbor, Graph<Cell> newGrid, Iterator<Cell> it){
+        if(cell.getState() == 1) {
+            boolean percolatedCell = false;
+            for (Cell c : neighbor) {
+                if (c.getState() == 2) {
+                    percolatedCell = true;
+                }
+            }
+            if (percolatedCell) {
+                it.next().setState(2);
+            } else {
+                it.next().setState(1);
+            }
+        }
+        else{
+            it.next().setState(0);
+        }
+    }
+}
