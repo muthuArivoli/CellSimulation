8ac09e8da64dcc2077729cb8578fe42a4a1a243f
Muthu Arivoli <ma381@duke.edu>
2020-02-02 02:04:18 -0500

    add segregation simulation

---
+++ src/simulation/SegregationSimulation.java
@@ -0,0 +1,28 @@
+package simulation;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class SegregationSimulation extends Simulation {
+    private double threshold = 0.3;
+    protected void getNextState(Cell cell, List<Cell> neighbor, Graph<Cell> newGrid, Iterator<Cell> it){
+        int similarNeighbors = 0;
+        for(Cell c:neighbor){
+            if(c.getState() == cell.getState()){
+                similarNeighbors++;
+            }
+        }
+        if((double)similarNeighbors/neighbor.size() <=threshold){
+            Iterator<Cell> initialIt = newGrid.getVertices().iterator();
+            for(Cell c:myGrid.getVertices()){
+                Cell thisCell = initialIt.next();
+                if(c.getState() == 0 && thisCell.getState() == 0){
+                    thisCell.setState(cell.getState());
+                    it.next().setState(0);
+                    return;
+                }
+            }
+        }
+        it.next();
+    }
+}
